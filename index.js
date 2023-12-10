5b6e4147d05f9ebe204fff89f9c0aa23a96735d0
omertztoya <omersr9@gmail.com>
2023-12-10 17:02:11 +0200

    v1

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+// database connection 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "ToDoProject",
+  password: "s676v3wR!",
+  port: 5432,
+});
+db.connect();
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Option 2" },
+  { id: 2, title: "Option 1" },
+];
+
+// routes for the app 
+app.get("/", async (req, res) => {
+try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+
+  res.render("index.ejs", {
+    listTitle: "To Do List",
+    listItems: items,
+  });
+} catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+  res.redirect("/");
+} catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+
+  try {
+    await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
