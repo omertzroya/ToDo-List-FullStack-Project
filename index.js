6f05238c44e1b0b861462c76fd0559b874160b02
omertztoya <omersr9@gmail.com>
2023-12-14 17:52:41 +0200

    v1.1

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 const app = express();
 const port = 3000;
 
+
 // database connection 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "YOUR_DATABASE_NAME",
-  password: "YOUR_PASSWORD",
+  user:process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 db.connect();
 
